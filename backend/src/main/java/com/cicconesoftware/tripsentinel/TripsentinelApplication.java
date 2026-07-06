2c88cca542c5ab4199b0f82948b05edf875f611b
Francesco-87 <cicconesoftwaresolutions@gmail.com>
2026-07-06 20:46:30 +0200

    Initialize Spring Boot backend with Maven and Java 25

---
+++ backend/src/main/java/com/cicconesoftware/tripsentinel/TripsentinelApplication.java
@@ -0,0 +1,13 @@
+package com.cicconesoftware.tripsentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripsentinelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripsentinelApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cicconesoftware/tripsentinel/TripsentinelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicconesoftware.tripsentinel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripsentinelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
